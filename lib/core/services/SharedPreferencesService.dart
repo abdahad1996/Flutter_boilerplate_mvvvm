d821724d4ff46418067177af4ae5f245e24e4dcf
abdulahad <>
2020-11-06 02:36:46 +0500

    -inapp purchase added

--- lib/core/services/SharedPreferencesService.dart
+++ lib/core/services/SharedPreferencesService.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-// import 'package:shared_preferences/shared_preferences.dart';
 // @singleton
 class SharedPreferencesService {
   static SharedPreferencesService _instance;
@@ -18,36 +17,6 @@ class SharedPreferencesService {
 
   final SharedPreferences _preferences;
   SharedPreferencesService._(this._preferences);
-
-  // //TODO: MOVE TO ANOTHER CLASS
-  // static const _ThemeIndexKey = 'user_key';
-  // static const _UserThemeModeKey = 'user_theme_mode_key';
-
-  // int get themeIndex => getFromDisk(_ThemeIndexKey);
-
-  // set themeIndex(int value) => saveToDisk(_ThemeIndexKey, value);
-
-  // ThemeMode get userThemeMode {
-  //   var userThemeString = getFromDisk(_UserThemeModeKey);
-  //   if (userThemeString == ThemeMode.dark.toString()) {
-  //     return ThemeMode.dark;
-  //   }
-
-  //   if (userThemeString == ThemeMode.light.toString()) {
-  //     return ThemeMode.light;
-  //   }
-
-  //   return null;
-  // }
-
-  // set userThemeMode(ThemeMode value) {
-  //   if (value == null) {
-  //     saveToDisk(_UserThemeModeKey, value);
-  //   } else {
-  //     var userTheme = value.toString();
-  //     saveToDisk(_UserThemeModeKey, userTheme);
-  //   }
-  // }
 
   //generic methods
   void clearPreferences() {
